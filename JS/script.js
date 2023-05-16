ce6ef309216aa0d8191422eb06862b3bc0068972
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-16 15:33:30 +0200

    add if and else if

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,21 @@ for (let i = 1; i <= 100; i++){
     console.log(i);
  
     if (i % 3 === 0) {
+
+
+        
          
+    } else if (i % 5 === 0){
+
+
+
+
+
+    } else if (i % 3 === 0 && (i % 5 === 0)){
+
+
+
+
+
     }
 }
