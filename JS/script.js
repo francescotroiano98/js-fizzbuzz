e8e41e9e8df0fd581de4b9a0a0d1d1957a93cbb2
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-16 15:53:58 +0200

    done pseudo soluction

--- JS/script.js
+++ JS/script.js
@@ -4,22 +4,25 @@ const ulelement = document.querySelector("ul");
 for (let i = 1; i <= 100; i++){
     console.log(i);
     ulelement.innerHTML += `<li>${i}</li>`;
-    if (i % 3 === 0) {
 
-    
+    if ((i % 3 === 0) && (i % 5 === 0)){
+
+        ulelement.innerHTML += `<li>${"Fizzbuzz"}</li>`;
+
+
+
+
+
+    } else if (i % 3 === 0) {
+
+        ulelement.innerHTML += `<li>${"fizz"}</li>`;
 
          
     } else if (i % 5 === 0){
 
+        ulelement.innerHTML += `<li>${"Buzz"}</li>`;
 
 
 
-
-    } else if (i % 3 === 0 && (i % 5 === 0)){
-
-
-
-
-
-    }
+    } 
 }
